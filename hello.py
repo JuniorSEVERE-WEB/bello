b06a66079658d7e7a2727ab6fd3e7882cf54698f
Junior SEVERE <120841577+JuniorSEVERE-WEB@users.noreply.github.com>
2023-09-20 08:27:35 -0400

    add cc

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 print("Hello, world!")
-<<<<<<< HEAD
-welcome to my website!!!
-modifier le fichier
-=======
 welcome to my website
 <h2 style="color:blue;">modifier le fichier</h2>
->>>>>>> a487776f5a380075384052b2c419f68d5c6f8d20
+
